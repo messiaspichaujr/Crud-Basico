61d982561684cb9403b73d2895d156c6eacbd7fe
Messias Ferreira Pichau <125512149+messiaspichaujr@users.noreply.github.com>
2024-05-16 10:41:27 -0300

    Add files via upload

--- controller/aluno_controller.js
+++ controller/aluno_controller.js
@@ -13,5 +13,58 @@ aluno.all = async function (req, res) {
         console.log("Erro na consulta...", e)
 
     }
+
 }
+
+aluno.create = async function (req, res){
+
+    try{
+        let aluno = req.body
+        let sql = "INSERT INTO aluno (matr_aluno, nome_aluno, email_aluno) values (?,?,?);"
+        let values = [aluno.matr_aluno, aluno.nome_aluno, aluno.email_aluno]
+        let result = await con.query(sql,values)
+        res.send({
+            status: "Inserção Efetuada com sucesso!",
+            result: result
+        })
+    
+    } catch (e) {
+        console.log("Erro........", e)
+
+    }
+}
+
+aluno.update = async function(req, res){
+    try{
+        let matricula = req.params.matr_aluno
+        let aluno = req.body
+        let sql = "UPDATE aluno SET nome_aluno=?, email_aluno=? WHERE matr_aluno=?;"
+        const values = [aluno.nome_aluno, aluno.email_aluno, matricula]
+        let result = await con.query(sql, values)
+        res.send({
+            status:"Atualização do : " + aluno.nome_aluno + aluno.email_aluno +  matricula,
+            result: result
+        })
+
+    }catch(e){
+        console.log("Erro....", e)
+    }
+}
+
+aluno.delete = async function(req, res){
+    try {
+        let matricula = req.params.matr_aluno
+        let sql = "DELETE FROM aluno WHERE matr_aluno=?"
+        let result = await con.query(sql, [matricula])
+        res.send({
+            status:"a exclusão do: " + matricula + "foi efetuada",
+            result: result
+        })
+
+    } catch (e) {
+        console.log("ERRO.......", e)
+    }
+}
+
 export {aluno}
+
--- routes/aluno_routes.js
+++ routes/aluno_routes.js
@@ -4,6 +4,10 @@ import {aluno} from "../controller/aluno_controller.js";
 let router = express.Router()
 
 router.get('/aluno',aluno.all)
-//definir as demais rotas para o restante do CRUD
+router.post('/aluno', aluno.create)
+router.put('/aluno/:matr_aluno', aluno.update)
+
+router.delete('/aluno/:matr_aluno', aluno.delete);
+//definir as demais rotas para Update e Delete
 
 export {router}
